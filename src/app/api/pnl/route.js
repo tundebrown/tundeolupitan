e845509f44faad7e60fc2c535dea83fc67d85ed6
tundebrown <121276415+tundebrown@users.noreply.github.com>
2025-04-15 17:04:02 +0100

    Update route.js

--- src/app/api/pnl/route.js
+++ src/app/api/pnl/route.js
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
+export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const image = searchParams.get('image');
 
